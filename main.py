10391c4246e59612210aff8de97970b5a1fc3027
Stan Slov <s.slov7@gmail.com>
2022-07-19 11:32:11 -0400

    Usage: when running server via Bash command: 'python main.py' ,

--- main.py
+++ main.py
@@ -9,15 +9,29 @@ as Link to the repository for the activity itself.
 ===============================================================
 """
 import os
+import time
 from flask import Flask
 
+# Usage: when running server via Bash command: 'python main.py' ,
+# then going to link provided at: "http://localhost:6787/time" ,
+# Resulting page displays --> "1658244420.5917065" or similar, 
+# as the current time in Epoch Timestamp in seconds.
 
 app = Flask(__name__)
 
+# with open('zipcodes.txt') as f:
+#     zipcodes = {}
+#     for line in f.readlines():
+#         # print(line)
+#         # print(line.strip())
+#         data = line.strip().split(',')
+#         zipcodes[data[0].strip()] = data[1:]
 
-@app.route('/')
-def home():
-    return "To Do"
+
+@app.route('/time')
+def get_coordinates():
+    # Returns the current time in seconds in Epoch Timestamp form
+    return str(time.time())
 
 
 if __name__ == '__main__':
